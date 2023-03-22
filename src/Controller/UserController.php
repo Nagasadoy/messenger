2dffa233d475487fcb51f9b847c0454d87135c93
Dmitry Borisov <nagasadoy@gmail.com>
2023-03-22 18:33:26 +0300

    study elastic search

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Services\ElasticSearch;
 use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,14 +16,37 @@ class UserController extends AbstractController
     }
 
     #[Route('api/users', methods: ['GET'])]
-    public function find(ElasticSearch $elasticSearch): Response
+    public function find(Request $request, ElasticSearch $elasticSearch): Response
     {
-//        $results = $this->finder->find('Alex');
-//        dd($results);
+        $query = $request->toArray()['query'] ?? '';
+        $elasticSearch->search('people_index', [
+            'match_phrase_prefix' => [
+                'comment' => $query
+            ]
+        ]);
 
-        $info = $elasticSearch->getInfo();
+//        $elasticSearch->search('people_index',[
+//            'bool' => [
+//                'should' => [
+//                    'term' => [
+//                        'comment' => 'план'
+//                    ]
+//                ]
+//            ]
+//        ]);
+
+//        $elasticSearch->addDocument('people_index', [
+//            'name' => 'alexey',
+//            'age' => 24,
+//            'comment' => 'Москва слезам не верит'
+//        ]);
+
+//        $elasticSearch->createIndex('people_index');
+//        $elasticSearch->deleteIndex('people_index');
+
+
         return $this->json([
-            'info' => $info
+            'message' => 'message'
         ]);
     }
 }
--- src/Services/ElasticSearch.php
+++ src/Services/ElasticSearch.php
@@ -2,11 +2,15 @@
 
 namespace App\Services;
 
+use App\Factory\UuidFactory;
 use Elasticsearch\ClientBuilder;
+use Elasticsearch\Common\Exceptions\ClientErrorResponseException;
+use Elasticsearch\Common\Exceptions\ServerErrorResponseException;
 
 class ElasticSearch
 {
     private $client;
+
     public function __construct()
     {
         $this->client = ClientBuilder::create()
@@ -18,5 +22,109 @@ class ElasticSearch
     {
         $response = $this->client->info();
         return $response['version']['number'];
+    }
+
+    public function deleteIndex(string $indexName): array
+    {
+        $response = $this->client->indices()->delete(['index' => $indexName]);
+        dd($response);
+
+    }
+
+    public function createIndex(string $indexName): array
+    {
+        $body = [
+            'settings' => [
+                'analysis' => [
+                    'analyzer' => [
+                        'my_analyzer' => [
+                            'tokenizer' => 'standard',
+                            'filter' => [
+                                'lowercase',
+                                'stop_words',
+                                'stemmer'
+                            ]
+                        ]
+                    ],
+                    'filter' => [
+                        'stop_words' => [
+                            'type' => 'stop',
+                            'stopwords' => '_russian_'
+                        ],
+                        'stemmer' => [
+                            'type' => 'stemmer',
+                            'language' => 'russian'
+                        ],
+                        'autocomplete_filter' => [
+                            'type' => 'edge_ngram',
+                            'min_gram' => 1,
+                            'max_gram' => 20
+                        ]
+                    ]
+                ]
+            ],
+            'mappings' => [
+                'properties' => [
+                    'name' => [
+                        'type' => 'text'
+                    ],
+                    'age' => [
+                        'type' => 'integer'
+                    ],
+                    'comment' => [
+                        'type' => 'text'
+                    ]
+                ]
+            ]
+        ];
+
+        try {
+            $response = $this->client->indices()->create([
+                'index' => $indexName,
+                'body' => $body
+            ]);
+        } catch (\Exception $e) {
+            throw new \DomainException($e->getMessage());
+        }
+        return $response;
+    }
+
+    public function addDocument(string $indexName, array $body): void
+    {
+        $uid = UuidFactory::generateNew();
+        $params = [
+            'index' => $indexName,
+            'body' => $body,
+            'id' => $uid
+        ];
+
+        try {
+            $response = $this->client->index($params);
+        } catch (ClientErrorResponseException|ServerErrorResponseException $e) {
+            throw new \DomainException($e->getMessage());
+        } catch (\Exception $e) {
+            throw new \DomainException('Неизвестная ошибка!');
+        }
+
+        dd($response);
+    }
+
+    public function search(string $index, array $query)
+    {
+        $params = [
+            'index' => $index,
+            'body' => [
+                'query' => $query
+            ]
+        ];
+
+        try {
+            $response = $this->client->search($params);
+        } catch (\Exception $exception) {
+            throw new \DomainException('Что-то пошло не так!');
+        }
+
+        $hits = $response['hits']['hits'] ?? [];
+        dd($hits);
     }
 }
