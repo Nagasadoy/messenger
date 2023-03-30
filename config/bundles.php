83635e80200cb52a06613536f1eff4a6e9dfb2f0
Dmitry Borisov <nagasadoy@gmail.com>
2023-03-30 18:46:26 +0300

    Генерация pdf документов для приложения к письмам, но почему-то пока не работает.

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
--- src/Command/SendEmailCommand.php
+++ src/Command/SendEmailCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use App\Model\User\Entity\User\UserRepository;
+use Knp\Snappy\Pdf;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -13,6 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
+use Twig\Environment;
 
 #[AsCommand(
     name: 'app:send-email',
@@ -23,6 +25,8 @@ class SendEmailCommand extends Command
     private readonly MailerInterface $mailer;
     public function __construct(
         private readonly UserRepository $userRepository,
+        private readonly Environment $twig,
+        private readonly Pdf $pdf,
         MailerInterface $mailer,
         string $name = null)
     {
@@ -40,11 +44,19 @@ class SendEmailCommand extends Command
         $io->progressStart(count($users));
 
         foreach ($users as $user) {
+
+            $html = $this->twig->render('email/report.html.twig', [
+                'name' => $user->getEmail()
+            ]);
+
+            $pdf = $this->pdf->getOutputFromHtml($html);
+
             $email = (new TemplatedEmail())
                 ->from(new Address('nagasadoy@gmail.com', 'Nagasadoy'))
                 ->to(new Address($user->getEmail(), $user->getEmail()))
                 ->subject('Test mail')
-                ->htmlTemplate('email/welcome.html.twig');
+                ->htmlTemplate('email/welcome.html.twig')
+                ->attach($pdf, sprintf('weekly-report-%s.pdf', date('Y-m-d')));
 
             $this->mailer->send($email);
 
